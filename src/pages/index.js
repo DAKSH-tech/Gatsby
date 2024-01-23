514ea67609ac54aba59b18d2ab289f9a8aa2336f
DAKSH-tech <daksh6796@gmail.com>
2024-01-23 13:03:49 +0530

    Learn gatsby-1

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+function index() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+        <div className="bg-white p-8 rounded shadow-md">
+            <Link to="/dakshNotes">
+            <h1 className="text-3xl font-bold mb-4">Daksh's Page</h1>
+            </Link>
+            <p className="text-gray-700">
+            Welcome to Daksh's page. Explore and enjoy!
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default index
